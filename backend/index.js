9ffb913bbbb6d736527b11edb216fa072cd9d5fe
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2023-04-04 13:01:22 -0600

    backend initialization

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import mysql from "mysql"
+
+const app = express()
+
+// const db = mysql.createConnection({
+//     host:"localhost",
+//     user:"",
+//     password:"",
+//     database:""
+// })
+
+app.get("/", (req, res) => {
+    res.json("hello this is the backend!")
+})
+
+app.get("/hello", (req, res) => {
+    res.json("yo")
+})
+
+app.listen(3000, () => {
+    console.log("backend initiated!")
+})
