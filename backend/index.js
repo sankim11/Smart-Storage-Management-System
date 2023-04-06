c86468193410f03de4f9619e622077b51d13489e
Saianeesh Vinmani <imsaianeesh17@gmail.com>
2023-04-06 16:27:46 -0600

    added first fetch query, not working yet

--- backend/index.js
+++ backend/index.js
@@ -4,19 +4,23 @@ import mysql from "mysql"
 const app = express()
 
 const port = 4000
-// const db = mysql.createConnection({
-//     host:"localhost",
-//     user:"",
-//     password:"",
-//     database:""
-// })
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"saianeesh",
+    password:"cpsc441",
+    database:"StorageSystem"
+})
 
 app.get("/", (req, res) => {
     res.json([{"test":"hello this is the backend!"}])
 })
 
-app.get("/hello", (req, res) => {
-    res.json("yo")
+app.get("/employees", (req, res) => {
+    const q = "SELECT * FROM employee"
+    db.query(q, (err,data) => {
+        if(err) return res.json(err)
+        return res.json(data)
+    })
 })
 
 app.listen(port, () => {
